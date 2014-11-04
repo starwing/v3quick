cc624a4715b5138be9339b6f10d51c47239e5b21
htl <htlxyz@126.com>
2014-11-04 09:56:12 +0800

    sync code form v3quickcocos

--- quick/framework/display.lua
+++ quick/framework/display.lua
@@ -72,7 +72,7 @@ local sharedAnimationCache   = cc.AnimationCache:getInstance()
 -- check device screen size
 local glview = sharedDirector:getOpenGLView()
 if nil == glview then
-    glview = cc.GLViewImpl:createWithRect("QuickCocos", 
+    glview = cc.GLViewImpl:createWithRect("QuickCocos",
         cc.rect(0, 0, CONFIG_SCREEN_WIDTH or 900, CONFIG_SCREEN_HEIGHT or 640))
     sharedDirector:setOpenGLView(glview)
 end
